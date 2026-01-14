b066a0de159407cf6b036a204a90b3402da36106
Clinton Moshe <clintonmwachia9@gmail.com>
2026-01-14 12:30:09 +0300

    feat: defined Months variable

--- helpers/globals.go
+++ helpers/globals.go
@@ -4,3 +4,5 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // Global variable to store the current user's ID
 var CurrentUserID primitive.ObjectID
+var Months = []string{"Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept",
+	"Oct", "Nov", "Dec"}
